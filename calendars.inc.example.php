7d6d915366acd073209834fb937be46ad1af220c
Nicholas Avenell <nicholas@istic.net>
2018-11-18 11:40:16 +0100

    Clean-up

--- calendars.inc.example.php
+++ calendars.inc.example.php
@@ -3,9 +3,10 @@
 
 $calendars = array(
 
-	'Holidays' => array(
-		'src' => "k6ihf65p5md3okg9fpu4r2q36qk80r7e@import.calendar.google.com",
-		'color' => '#865A5A'
+	'holidays' => array( // Unique identifier
+		'name' => 'Holidays in the UK', // Name to display in the key
+		'src' => "k6ihf65p5md3okg9fpu4r2q36qk80r7e@import.calendar.google.com", // GCal UID
+		'color' => '#865A5A' // Hexidecimal Hue
 		),
 	// '' => array(
 	// 	'src' => "",
