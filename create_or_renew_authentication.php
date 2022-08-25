53a467cc8f09c1babc9ec927a09df01cb814b8c7
Nicholas Avenell <nicholas@istic.net>
2022-08-25 15:16:44 +0000

    Update the world

--- create_or_renew_authentication.php
+++ create_or_renew_authentication.php
@@ -2,9 +2,6 @@
 require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/lib/gcal.lib.php';
 
-use Google\Photos\Library\V1\PhotosLibraryClient;
-use Google\Photos\Library\V1\PhotosLibraryResourceFactory;
-
 if (php_sapi_name() != 'cli') {
     throw new Exception('This application must be run on the command line.');
 }
@@ -14,22 +11,6 @@ if (php_sapi_name() != 'cli') {
 $client = getClient();
 
 
-$photosLibraryClient = new PhotosLibraryClient(['credentials' => getGoogleCreds()]);
-
-
-$response = $photosLibraryClient->listMediaItems();
-foreach ($response->iterateAllElements() as $item) {
-    // Get some properties of a media item
-    /* @var $item \Google\Photos\Library\V1\MediaItem */
-    $id = $item->getId();
-    $description = $item->getDescription();
-    $mimeType = $item->getMimeType();
-    $productUrl = $item->getProductUrl();
-    $filename = $item->getFilename();
-    print($filename.' '.$description."\n");
-
-    break;
-}
 
 $service = new Google_Service_Calendar($client);
 
