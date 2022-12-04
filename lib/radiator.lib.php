47d282617671129eff39bb708d5e7258189871a6
Nicholas Avenell <nicholas@istic.net>
2022-12-04 15:32:59 +0000

    🛠 Lint

--- lib/radiator.lib.php
+++ lib/radiator.lib.php
@@ -13,14 +13,15 @@ $sun_info = date_sun_info(time(), MY_LAT, MY_LON);
 $set = $sun_info['sunset'];
 $rise = $sun_info['sunrise'];
 
-if (time() > $set || time() < $rise ) {
+if (time() > $set || time() < $rise) {
     define("THEME", "nighttime");
 } else {
     define("THEME", "daytime");
 }
 
 
-function adjustBrightness($hex, $steps) {
+function adjustBrightness($hex, $steps)
+{
     // From https://stackoverflow.com/questions/3512311/how-to-generate-lighter-darker-color-with-php
 
     // Steps should be between -255 and 255. Negative = darker, positive = lighter
@@ -29,7 +30,7 @@ function adjustBrightness($hex, $steps) {
     // Normalize into a six character long hex string
     $hex = str_replace('#', '', $hex);
     if (strlen($hex) == 3) {
-        $hex = str_repeat(substr($hex,0,1), 2).str_repeat(substr($hex,1,1), 2).str_repeat(substr($hex,2,1), 2);
+        $hex = str_repeat(substr($hex, 0, 1), 2).str_repeat(substr($hex, 1, 1), 2).str_repeat(substr($hex, 2, 1), 2);
     }
 
     // Split into three parts: R, G and B
@@ -38,21 +39,22 @@ function adjustBrightness($hex, $steps) {
 
     foreach ($color_parts as $color) {
         $color   = hexdec($color); // Convert to decimal
-        $color   = max(0,min(255,$color + $steps)); // Adjust color
+        $color   = max(0, min(255, $color + $steps)); // Adjust color
         $return .= str_pad(dechex($color), 2, '0', STR_PAD_LEFT); // Make two char hex code
     }
 
     return $return;
 }
 
-function fullcal_json($google_calendars){
+function fullcal_json($google_calendars)
+{
     $output = [];
-    foreach($google_calendars as $id => $calendar){
+    foreach ($google_calendars as $id => $calendar) {
         $line = array(
             'url' => 'calendar.php?cal='.$calendar['src'],
             'className' => 'cal-'.$id,
         );
-        if(isset($calendar['rendering'])){
+        if (isset($calendar['rendering'])) {
             $line['rendering'] = $calendar['rendering'];
             $line['backgroundColor'] = $calendar['color'];
         }
@@ -101,36 +103,37 @@ function checkEmoji($str)
     return true;
 }
 
-function removeEmoji($text) {
-
+function removeEmoji($text)
+{
     return preg_replace('/([0-9|#][\x{20E3}])|[\x{00ae}|\x{00a9}|\x{203C}|\x{2047}|\x{2048}|\x{2049}|\x{3030}|\x{303D}|\x{2139}|\x{2122}|\x{3297}|\x{3299}][\x{FE00}-\x{FEFF}]?|[\x{2190}-\x{21FF}][\x{FE00}-\x{FEFF}]?|[\x{2300}-\x{23FF}][\x{FE00}-\x{FEFF}]?|[\x{2460}-\x{24FF}][\x{FE00}-\x{FEFF}]?|[\x{25A0}-\x{25FF}][\x{FE00}-\x{FEFF}]?|[\x{2600}-\x{27BF}][\x{FE00}-\x{FEFF}]?|[\x{2900}-\x{297F}][\x{FE00}-\x{FEFF}]?|[\x{2B00}-\x{2BF0}][\x{FE00}-\x{FEFF}]?|[\x{1F000}-\x{1F6FF}][\x{FE00}-\x{FEFF}]?/u', '', $text);
-
 }
 
-register_shutdown_function( "check_for_fatal" );
+register_shutdown_function("check_for_fatal");
 
-function log_error( $num, $str, $file, $line, $context = null )
+function log_error($num, $str, $file, $line, $context = null)
 {
-    log_exception( new ErrorException( $str, 0, $num, $file, $line ) );
+    log_exception(new ErrorException($str, 0, $num, $file, $line));
 }
 function check_for_fatal()
 {
     $error = error_get_last();
-    if (! $error ){
-    return;
+    if (! $error) {
+        return;
     }
-    if ( $error["type"] == E_ERROR )
-        log_error( $error["type"], $error["message"], $error["file"], $error["line"] );
+    if ($error["type"] == E_ERROR) {
+        log_error($error["type"], $error["message"], $error["file"], $error["line"]);
+    }
 }
-function log_exception($e){
-    if (!is_a($e, 'Exception')){
+function log_exception($e)
+{
+    if (!is_a($e, 'Exception')) {
         var_dump($e);
         debug_print_backtrace();
         die();
     }
-    if(defined('SEND_JSON_ERRORS') && SEND_JSON_ERRORS == true ){
+    if (defined('SEND_JSON_ERRORS') && SEND_JSON_ERRORS == true) {
         send_json_error($e->getCode(), get_class($e).": ".$e->getMessage(), $e->getFile(), $e->getLine(), $e);
-    } elseif(defined('SEND_TEXT_ERRORS') && SEND_TEXT_ERRORS == true ){
+    } elseif (defined('SEND_TEXT_ERRORS') && SEND_TEXT_ERRORS == true) {
         send_text_error($e->getCode(), $e->getMessage(), $e->getFile(), $e->getLine());
     } else {
         $file = str_replace(realpath(getcwd().DIRECTORY_SEPARATOR.'..'), '', $e->getFile());
@@ -139,7 +142,7 @@ function log_exception($e){
         print "<div style='text-align: center;'>";
         print "<h2 style='color: rgb(190, 50, 50);'>Exception Occured:</h2>";
         print "<table style='width: 800px; display: inline-block;'>";
-        print "<tr style='background-color:rgb(230,230,230);'><th style='width: 80px;'>Type</th><td>" . get_class( $e ) . "</td></tr>";
+        print "<tr style='background-color:rgb(230,230,230);'><th style='width: 80px;'>Type</th><td>" . get_class($e) . "</td></tr>";
         print "<tr style='background-color:rgb(240,240,240);'><th>Message</th><td>{$e->getMessage()}</td></tr>";
         print "<tr style='background-color:rgb(230,230,230);'><th>File</th><td>{$file}</td></tr>";
         print "<tr style='background-color:rgb(240,240,240);'><th>Line</th><td>{$e->getLine()}</td></tr>";
@@ -147,7 +150,8 @@ function log_exception($e){
         die();
     }
 }
-function send_json_error($errno, $errstr, $errfile, $errline, $e = false){
+function send_json_error($errno, $errstr, $errfile, $errline, $e = false)
+{
     $file = str_replace(realpath(getcwd().DIRECTORY_SEPARATOR.'..'), '', $errfile);
     header('HTTP/1.1 500 Something Bad');
     header('content-type: application/json; charset: utf-8');
@@ -159,14 +163,13 @@ function send_json_error($errno, $errstr, $errfile, $errline, $e = false){
     die();
     // throw new ErrorException($errstr, $errno, 0, $errfile, $errline);
 }
-function send_text_error($errno, $errstr, $errfile, $errline ){
+function send_text_error($errno, $errstr, $errfile, $errline)
+{
     $file = str_replace(realpath(getcwd().DIRECTORY_SEPARATOR.'..'), '', $errfile);
     print "{$errstr} in {$file} at {$errline}";
-        debug_print_backtrace();
+    debug_print_backtrace();
     die();
     // throw new ErrorException($errstr, $errno, 0, $errfile, $errline);
 }
 set_error_handler('log_error');
-set_exception_handler( "log_exception" );
-
-
+set_exception_handler("log_exception");
