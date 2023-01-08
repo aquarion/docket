924b356964bf1452bcfe41adbed99a8c1842b1a5
Nicholas Avenell <nicholas@istic.net>
2023-01-08 16:12:04 +0000

    Update the example

--- calendars.inc.example.php
+++ calendars.inc.example.php
@@ -1,5 +1,27 @@
 <?php
 
+/**
+ * File: Config.example.inc.php
+ *
+ * Includeable file containing calendar config
+ *
+ * @category config
+ * @package  Docket
+ * @author   aquarion <hello@istic.net>
+ * @license  n/a
+ * @link     https://github.com/aquarion/docket
+ */
+
+
+
+define("MAPBOX_API_TOKEN", "XXX"); //
+
+define("REDIS_HOST", false); // Leave this false if you don't have redis
+
+// define("REDIS_HOST", "172.17.0.1");
+// define("REDIS_PORT", "6379");
+// define("REDIS_PASSWORD", false);
+
 
 $google_calendars = array(
 
@@ -13,3 +35,27 @@ $google_calendars = array(
     //     'color' => '#'
     //     ),
 );
+
+$ical_calendars = array(
+    'uniq_id' => array(
+        'name' => "Display Name",
+        'src'  => "ICal URL",
+        'color' => '#0096ff',
+        'emoji' => '🌀'
+    ),
+
+);
+
+
+// merge ids from the above with a dash between them to define a new colour for shared events
+$merged_calendars = array(
+    "aquarion-fyr" => array(
+        'color' => "#8347c6"
+    )
+);
+
+$filter_out = array();
+
+// FOr distance calculations
+define("MY_LAT", "1");
+define("MY_LON", "1");
