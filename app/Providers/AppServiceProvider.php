ffe569f785727d4eeff940a0276384e50114c21e
Nicholas Avenell <nicholas@istic.net>
2026-02-01 14:23:08 +0000

    Implement configuration overrides loading in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,34 @@ class AppServiceProvider extends ServiceProvider
    */
   public function boot(): void
   {
-    //
+    $this->loadConfigOverrides();
+  }
+
+  /**
+   * Load configuration overrides from etc/config/ if they exist.
+   */
+  protected function loadConfigOverrides(): void
+  {
+    $overrideDir = base_path('etc/config');
+
+    if (! is_dir($overrideDir)) {
+      return;
+    }
+
+    $configFiles = glob($overrideDir . '/*.php');
+
+    foreach ($configFiles as $file) {
+      $configName = basename($file, '.php');
+      $overrideConfig = require $file;
+
+      if (is_array($overrideConfig)) {
+        config([
+          $configName => array_replace_recursive(
+            config($configName, []),
+            $overrideConfig
+          ),
+        ]);
+      }
+    }
   }
 }
