f485da4fc5bcbae20ef6fe8b0e3573f0db8ca966
Nicholas Avenell <nicholas@istic.net>
2026-02-01 20:20:51 +0000

    Move JS files into resources to be bundled by vite

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,10 +10,10 @@ window.twemoji = twemoji;
 
 // Import application JavaScript files
 // These files depend on the global variables set above
-import "../../public/static/js/date-utils.js";
-import "../../public/static/js/notification-utils.js";
-import "../../public/static/js/circle-progress.js";
-import "../../public/static/js/docket-ui.js";
-import "../../public/static/js/docket-calendar.js";
-import "../../public/static/js/docket-events.js";
-import "../../public/static/js/docket-main.js";
+import "./date-utils.js";
+import "./notification-utils.js";
+import "./circle-progress.js";
+import "./docket-ui.js";
+import "./docket-calendar.js";
+import "./docket-events.js";
+import "./docket-main.js";
