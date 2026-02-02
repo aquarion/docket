15542d2fb2300f8b637db120b8c7079bec3a4b6e
Nicholas Avenell <nicholas@learningonscreen.ac.uk>
2026-02-02 00:27:50 +0000

    feat: add custom 403 error pages with countdown refresh and IP blocking message

---
+++ public/errors/error.403.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>403 Forbidden</title>
+  <meta http-equiv="refresh" content="30"> <!-- Auto refresh every 30 seconds -->
+  <style src="static/css/style.css"></style>
+  </style>
+</head>
+
+<body>
+  <h1>Forbidden</h1>
+  <p>Docket is IP blocked from your current location (<?php echo $_SERVER['HTTP_X_FORWARDED_FOR']; ?>). This page will refresh in <span id="countdown">30</span> seconds</p>
+  <script>
+    var countdownElement = document.getElementById("countdown");
+    var countdown = 30;
+    var countdownInterval = setInterval(function() {
+      countdown--;
+      countdownElement.textContent = countdown;
+      if (countdown <= 0) {
+        clearInterval(countdownInterval);
+        location.reload();
+      }
+    }, 1000);
+  </script>
+  <p><a href="/">Return to Home</a></p>
+</body>
+
+</html>
